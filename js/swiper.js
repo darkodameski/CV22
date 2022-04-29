6f2b120eb1d7fe2ee1893d3a2ca6b0c93497a8ff
Darko Dameski <vandit_pp@hotmail.com>
2022-04-29 16:01:33 +0200

    New code update

---
+++ js/swiper.js
@@ -0,0 +1,11 @@
+let swiper = new Swiper(".portfolio__container", {
+
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+
+    mousewheel: true,
+    keyboard: true,
+});
